839604bb518b0e46b12ed3829a9a3c9871b7ba26
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-10-17 18:56:11 -0700

    import bootstrap build scripts etc

---
+++ bootstrap/build/static_artifacts/master-node/aws/terraform/modules/etcd-volume-attachment/etcd-volume-attachment.tf
@@ -0,0 +1,46 @@
+###############################################################################
+# Variables
+
+variable "lattice_id" {}
+variable "system_id" {}
+variable "region" {}
+variable "name" {}
+variable "instance_id" {}
+variable "device_name" {}
+
+###############################################################################
+# Data
+
+data "aws_ebs_volume" "etcd_data_volume" {
+  most_recent = true
+
+  filter {
+    name   = "tag:Name"
+    values = ["lattice.system.${var.system_id}.master-${var.name}-etcd"]
+  }
+}
+
+###############################################################################
+# Backend
+
+terraform {
+  backend "s3" {
+    encrypt = true
+  }
+}
+
+###############################################################################
+# Provider
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+###############################################################################
+# EBS
+
+resource "aws_volume_attachment" "etcd_data_volume_attachment" {
+  device_name = "${var.device_name}"
+  volume_id   = "${data.aws_ebs_volume.etcd_data_volume.id}"
+  instance_id = "${var.instance_id}"
+}
