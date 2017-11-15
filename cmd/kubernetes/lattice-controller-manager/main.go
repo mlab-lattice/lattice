a362b6be386c723d7760c8601ddd6df86b9472d9
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-11-15 13:04:03 -0800

    update terraform module path

--- cmd/kubernetes/lattice-controller-manager/main.go
+++ cmd/kubernetes/lattice-controller-manager/main.go
@@ -7,15 +7,18 @@ import (
 )
 
 var (
-	kubeconfig string
-	provider   string
+	kubeconfig          string
+	provider            string
+	terraformModulePath string
 )
 
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to kubeconfig file")
+	flag.StringVar(&provider, "provider", "", "provider to use")
+	flag.StringVar(&terraformModulePath, "terraform-module-path", "/etc/terraform/modules", "path to terraform modules")
 	flag.Parse()
 }
 
 func main() {
-	app.Run(kubeconfig, provider)
+	app.Run(kubeconfig, provider, terraformModulePath)
 }
