4d5392f80ce2d3ae2c6dedc308578edb2a044bae
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-12-04 16:31:32 -0800

    update pre-push hook

--- scripts/git/pre-push.sh
+++ scripts/git/pre-push.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LATTICE_ROOT=${DIR}/../..
 cd ${LATTICE_ROOT}
 
 echo "Running \"make build\"..."
-make build
+make build > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then echo "\"make build\" failed" && exit 1; fi
 
-echo
+set -e
 echo "Running \"make test\"..."
 make test
