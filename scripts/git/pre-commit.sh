c5a47dded99b1c48f4da0656e2bfdafb9e4e337c
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-12-04 16:22:42 -0800

    update git hooks

--- scripts/git/pre-commit.sh
+++ scripts/git/pre-commit.sh
@@ -6,10 +6,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LATTICE_ROOT=${DIR}/../..
 cd ${LATTICE_ROOT}
 
-GAZELLE_ERROR_MSG="Please run \"make gazelle\" prior to committing"
+echo "Checking BUILD files and formatting..."
+
+GAZELLE_ERROR_MSG="Please run \"make gazelle\" and add the generated BUILD.bazel files prior to committing"
 [[ $(bazel run -- //:gazelle -mode diff 2>/dev/null) ]] && echo ${GAZELLE_ERROR_MSG} && exit 1
 
-FMT_ERROR_MSG="Please run \"make format\" prior to committing"
+FMT_ERROR_MSG="Please run \"make format\" and add the fixed files prior to committing"
 [[ $(gofmt -l .) ]] && echo ${FMT_ERROR_MSG} && exit 1
 [[ $(terraform fmt -list -write=false .) ]] && echo ${FMT_ERROR_MSG} && exit 1
 
--- scripts/git/pre-push.sh
+++ scripts/git/pre-push.sh
@@ -6,5 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LATTICE_ROOT=${DIR}/../..
 cd ${LATTICE_ROOT}
 
+echo "Running \"make build\"..."
 make build
+
+echo
+echo "Running \"make test\"..."
 make test
