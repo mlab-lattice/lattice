7bc7e86aeece22e5b999fc20813ce4d8b85f9353
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-12-11 11:33:07 -0800

    refactor kuberentes bootstrap command

--- cmd/cli/admin/app/kubernetes/bootstrap/cmd.go
+++ cmd/cli/admin/app/kubernetes/bootstrap/cmd.go
@@ -11,7 +11,6 @@ import (
 	cloudboostrapper "github.com/mlab-lattice/system/pkg/backend/kubernetes/lifecycle/bootstrapper/cloud"
 	kubeutil "github.com/mlab-lattice/system/pkg/backend/kubernetes/util/kubernetes"
 	"github.com/mlab-lattice/system/pkg/constants"
-	"github.com/mlab-lattice/system/pkg/types"
 	"github.com/mlab-lattice/system/pkg/util/cli"
 
 	"k8s.io/client-go/rest"
@@ -21,43 +20,39 @@ import (
 )
 
 var (
-	dryRun              bool
-	kubeconfigPath      string
-	kubeconfigContext   string
-	kubeNamespacePrefix string
+	kubeconfigPath    string
+	kubeconfigContext string
 
-	latticeControllerManagerImage       string
-	latticeControllerManagerArgs        []string
 	defaultLatticeControllerManagerArgs = []string{
 		"-v", "5",
 		"-logtostderr",
 	}
 
-	managerAPIImage       string
-	managerAPIPort        int32
-	managerAPIHostNetwork bool
-	managerAPIArgs        []string
 	defaultManagerAPIArgs = []string{}
-
-	componentBuilderImage            string
-	componentBuilderDockerAPIVersion string
-
-	componentBuildDockerArtifactRegistry           string
-	componentBuildDockerArtifactRepositoryPerImage bool
-	componentBuildDockerArtifactRepository         string
-	componentBuildDockerArtifactPush               bool
-
-	envoyPrepareImage      string
-	envoyImage             string
-	envoyRedirectCIDRBlock string
-	envoyXDSAPIPort        int32
 
 	provider     string
 	providerVars []string
 
 	terraformBackend     string
 	terraformBackendVars []string
+
+	networkingProvider     string
+	networkingProviderVars []string
 )
+
+var options = &bootstrapper.Options{
+	Config: crv1.ConfigSpec{
+		ComponentBuild: crv1.ConfigComponentBuild{
+			Builder:        crv1.ConfigComponentBuildBuilder{},
+			DockerArtifact: crv1.ConfigComponentBuildDockerArtifact{},
+		},
+		Envoy: crv1.ConfigEnvoy{},
+	},
+	MasterComponents: baseboostrapper.MasterComponentOptions{
+		LatticeControllerManager: baseboostrapper.LatticeControllerManagerOptions{},
+		ManagerAPI:               baseboostrapper.ManagerAPIOptions{},
+	},
+}
 
 var Cmd = &cobra.Command{
 	Use:   "bootstrap",
@@ -65,12 +60,12 @@ var Cmd = &cobra.Command{
 	// FIXME: figure out why it thinks two args are getting passed in
 	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		if !componentBuildDockerArtifactRepositoryPerImage && componentBuildDockerArtifactRepository == "" {
+		if !options.Config.ComponentBuild.DockerArtifact.RepositoryPerImage && options.Config.ComponentBuild.DockerArtifact.Repository == "" {
 			panic("must specify component-build-docker-artifact-repository if not component-build-docker-artifact-repository-per-image")
 		}
 
 		var kubeconfig *rest.Config
-		if !dryRun {
+		if !options.DryRun {
 			var err error
 			kubeconfig, err = kubeutil.NewConfig(kubeconfigPath, "")
 			if err != nil {
@@ -82,54 +77,19 @@ var Cmd = &cobra.Command{
 		if err != nil {
 			panic(err)
 		}
+		options.Config.Provider = *providerConfig
 
 		terraformConfig, err := parseTerraformVars()
 		if err != nil {
 			panic(err)
 		}
+		options.Config.Terraform = terraformConfig
 
-		options := &bootstrapper.Options{
-			DryRun: dryRun,
-			Config: crv1.ConfigSpec{
-				KubernetesNamespacePrefix: kubeNamespacePrefix,
-				ComponentBuild: crv1.ConfigComponentBuild{
-					Builder: crv1.ConfigComponentBuildBuilder{
-						Image:            componentBuilderImage,
-						DockerAPIVersion: componentBuilderDockerAPIVersion,
-					},
-					DockerArtifact: crv1.ConfigComponentBuildDockerArtifact{
-						Registry:           componentBuildDockerArtifactRegistry,
-						RepositoryPerImage: componentBuildDockerArtifactRepositoryPerImage,
-						Repository:         componentBuildDockerArtifactRepository,
-						Push:               componentBuildDockerArtifactPush,
-					},
-				},
-				Envoy: crv1.ConfigEnvoy{
-					PrepareImage:      envoyPrepareImage,
-					Image:             envoyImage,
-					RedirectCIDRBlock: envoyRedirectCIDRBlock,
-					XDSAPIPort:        envoyXDSAPIPort,
-				},
-				SystemConfigs: map[types.LatticeNamespace]crv1.ConfigSystem{},
-				Provider:      *providerConfig,
-				Terraform:     terraformConfig,
-			},
-			MasterComponents: baseboostrapper.MasterComponentOptions{
-				LatticeControllerManager: baseboostrapper.LatticeControllerManagerOptions{
-					Image: latticeControllerManagerImage,
-					Args:  latticeControllerManagerArgs,
-				},
-				ManagerAPI: baseboostrapper.ManagerAPIOptions{
-					Image:       managerAPIImage,
-					Port:        managerAPIPort,
-					HostNetwork: managerAPIHostNetwork,
-					Args:        managerAPIArgs,
-				},
-			},
-			Networking: &cloudboostrapper.NetworkingOptions{
-				Flannel: &cloudboostrapper.FlannelOptions{},
-			},
+		networkingOptions, err := parseNetworkingVars()
+		if err != nil {
+			panic(err)
 		}
+		options.Networking = networkingOptions
 
 		b, err := bootstrapper.NewBootstrapper(options, kubeconfig)
 		if err != nil {
@@ -141,7 +101,7 @@ var Cmd = &cobra.Command{
 			panic(err)
 		}
 
-		if dryRun {
+		if options.DryRun {
 			for idx, object := range objects {
 				if idx != 0 {
 					fmt.Println("---")
@@ -157,44 +117,48 @@ var Cmd = &cobra.Command{
 }
 
 func init() {
-	Cmd.Flags().BoolVar(&dryRun, "dry-run", false, "if set, will not actually bootstrap the cluster but will instead print out the resources needed to bootstrap the cluster")
+	Cmd.Flags().BoolVar(&options.DryRun, "dry-run", false, "if set, will not actually bootstrap the cluster but will instead print out the resources needed to bootstrap the cluster")
 	Cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", "", "path to kubeconfig")
 	Cmd.Flags().StringVar(&kubeconfigContext, "kubeconfig-context", "", "context in the kubeconfig to use")
 
-	Cmd.Flags().StringVar(&kubeNamespacePrefix, "namespace-prefix", "lattice", "prefix to add to namespaces that lattice will create and own")
+	Cmd.Flags().StringVar(&options.Config.KubernetesNamespacePrefix, "namespace-prefix", "lattice", "prefix to add to namespaces that lattice will create and own")
 
-	Cmd.Flags().StringVar(&latticeControllerManagerImage, "lattice-controller-manager-image", "", "docker image to user for the lattice-controller-manager")
-	Cmd.MarkFlagRequired("lattice-controller-manager-image")
-	Cmd.Flags().StringArrayVar(&latticeControllerManagerArgs, "lattice-controller-manager-args", defaultLatticeControllerManagerArgs, "extra arguments (besides --provider) to pass to the lattice-controller-manager")
-
-	Cmd.Flags().StringVar(&managerAPIImage, "manager-api-image", "", "docker image to user for the lattice-controller-manager")
-	Cmd.MarkFlagRequired("manager-api-image")
-	Cmd.Flags().Int32Var(&managerAPIPort, "manager-api-port", 80, "port that the manager-api should listen on")
-	Cmd.Flags().BoolVar(&managerAPIHostNetwork, "manager-api-host-network", true, "whether or not the manager-api should be on the host network")
-	Cmd.Flags().StringArrayVar(&managerAPIArgs, "manager-api-args", defaultManagerAPIArgs, "extra arguments (besides --provider) to pass to the lattice-controller-manager")
-
-	Cmd.Flags().StringVar(&componentBuilderImage, "component-builder-image", "", "docker image to user for the component-builder")
+	Cmd.Flags().StringVar(&options.Config.ComponentBuild.Builder.Image, "component-builder-image", "", "docker image to user for the component-builder")
 	Cmd.MarkFlagRequired("component-builder-image")
-	Cmd.Flags().StringVar(&componentBuilderDockerAPIVersion, "component-builder-docker-api-version", "", "version of the docker API used by the build node docker daemon")
+	Cmd.Flags().StringVar(&options.Config.ComponentBuild.Builder.DockerAPIVersion, "component-builder-docker-api-version", "", "version of the docker API used by the build node docker daemon")
 
-	Cmd.Flags().StringVar(&componentBuildDockerArtifactRegistry, "component-build-docker-artifact-registry", "", "registry to tag component build docker artifacts with")
+	Cmd.Flags().StringVar(&options.Config.ComponentBuild.DockerArtifact.Registry, "component-build-docker-artifact-registry", "", "registry to tag component build docker artifacts with")
 	Cmd.MarkFlagRequired("component-build-docker-artifact-registry")
-	Cmd.Flags().BoolVar(&componentBuildDockerArtifactRepositoryPerImage, "component-build-docker-artifact-repository-per-image", false, "if false, one repository with a new tag for each artifact will be use, if true a new repository for each artifact will be used")
-	Cmd.Flags().StringVar(&componentBuildDockerArtifactRepository, "component-build-docker-artifact-repository", "", "repository to tag component build docker artifacts with, required if component-build-docker-artifact-repository-per-image is false")
-	Cmd.Flags().BoolVar(&componentBuildDockerArtifactPush, "component-build-docker-artifact-push", true, "whether or not the component-builder should push the docker artifact (use false for local)")
+	Cmd.Flags().BoolVar(&options.Config.ComponentBuild.DockerArtifact.RepositoryPerImage, "component-build-docker-artifact-repository-per-image", false, "if false, one repository with a new tag for each artifact will be use, if true a new repository for each artifact will be used")
+	Cmd.Flags().StringVar(&options.Config.ComponentBuild.DockerArtifact.Repository, "component-build-docker-artifact-repository", "", "repository to tag component build docker artifacts with, required if component-build-docker-artifact-repository-per-image is false")
+	Cmd.Flags().BoolVar(&options.Config.ComponentBuild.DockerArtifact.Push, "component-build-docker-artifact-push", true, "whether or not the component-builder should push the docker artifact (use false for local)")
 
-	Cmd.Flags().StringVar(&envoyPrepareImage, "envoy-prepare-image", "", "image to use for envoy-prepare")
+	Cmd.Flags().StringVar(&options.Config.Envoy.PrepareImage, "envoy-prepare-image", "", "image to use for envoy-prepare")
 	Cmd.MarkFlagRequired("envoy-prepare-image")
-	Cmd.Flags().StringVar(&envoyImage, "envoy-image", "envoyproxy/envoy-alpine", "image to use for envoy")
-	Cmd.Flags().StringVar(&envoyRedirectCIDRBlock, "envoy-redirect-cidr-block", "", "CIDR block to use to redirect traffic to envoy")
+	Cmd.Flags().StringVar(&options.Config.Envoy.Image, "envoy-image", "envoyproxy/envoy-alpine", "image to use for envoy")
+	Cmd.Flags().StringVar(&options.Config.Envoy.RedirectCIDRBlock, "envoy-redirect-cidr-block", "", "CIDR block to use to redirect traffic to envoy")
 	Cmd.MarkFlagRequired("envoy-redirect-cidr-block")
-	Cmd.Flags().Int32Var(&envoyXDSAPIPort, "envoy-xds-api-port", 8080, "port that the envoy-xds-api should listen on")
+	Cmd.Flags().Int32Var(&options.Config.Envoy.XDSAPIPort, "envoy-xds-api-port", 8080, "port that the envoy-xds-api should listen on")
+
+	Cmd.Flags().StringVar(&options.MasterComponents.LatticeControllerManager.Image, "lattice-controller-manager-image", "", "docker image to user for the lattice-controller-manager")
+	Cmd.MarkFlagRequired("lattice-controller-manager-image")
+	Cmd.Flags().StringArrayVar(&options.MasterComponents.LatticeControllerManager.Args, "lattice-controller-manager-args", defaultLatticeControllerManagerArgs, "extra arguments (besides --provider) to pass to the lattice-controller-manager")
+
+	Cmd.Flags().StringVar(&options.MasterComponents.ManagerAPI.Image, "manager-api-image", "", "docker image to user for the lattice-controller-manager")
+	Cmd.MarkFlagRequired("manager-api-image")
+	Cmd.Flags().Int32Var(&options.MasterComponents.ManagerAPI.Port, "manager-api-port", 80, "port that the manager-api should listen on")
+	Cmd.Flags().BoolVar(&options.MasterComponents.ManagerAPI.HostNetwork, "manager-api-host-network", true, "whether or not the manager-api should be on the host network")
+	Cmd.Flags().StringArrayVar(&options.MasterComponents.ManagerAPI.Args, "manager-api-args", defaultManagerAPIArgs, "extra arguments (besides --provider) to pass to the lattice-controller-manager")
 
 	Cmd.Flags().StringVar(&provider, "provider", "", "provider that the cluster is being bootstrapped on")
+	Cmd.MarkFlagRequired("provider")
 	Cmd.Flags().StringArrayVar(&providerVars, "provider-var", nil, "additional variables for the provider")
 
 	Cmd.Flags().StringVar(&terraformBackend, "terraform-backend", "", "backend to use for terraform")
 	Cmd.Flags().StringArrayVar(&terraformBackendVars, "terraform-backend-var", nil, "additional variables for the terraform backend")
+
+	Cmd.Flags().StringVar(&networkingProvider, "networking-provider", "", "provider to use for networking")
+	Cmd.Flags().StringArrayVar(&networkingProviderVars, "networking-provider-var", nil, "additional variables for the networking provider")
 }
 
 func parseProviderVars() (*crv1.ConfigProvider, error) {
@@ -327,4 +291,45 @@ func parseTerraformVarsS3() (*crv1.ConfigTerraformBackendS3, error) {
 		return nil, err
 	}
 	return s3Config, nil
+}
+
+func parseNetworkingVars() (*cloudboostrapper.NetworkingOptions, error) {
+	if networkingProvider == "" {
+		return nil, nil
+	}
+
+	var options *cloudboostrapper.NetworkingOptions
+	switch terraformBackend {
+	case kubeconstants.NetworkingProviderFlannel:
+		flannelOptions, err := parseNetworkingVarsFlannel()
+		if err != nil {
+			return nil, err
+		}
+		options = &cloudboostrapper.NetworkingOptions{
+			Flannel: flannelOptions,
+		}
+	default:
+		return nil, fmt.Errorf("unsupported networking provider: %v", networkingProvider)
+	}
+
+	return options, nil
+}
+
+func parseNetworkingVarsFlannel() (*cloudboostrapper.FlannelOptions, error) {
+	flannelOptions := &cloudboostrapper.FlannelOptions{}
+	flags := cli.EmbeddedFlag{
+		Target: &flannelOptions,
+		Expected: map[string]cli.EmbeddedFlagValue{
+			"network-cidr-block": {
+				Required:     true,
+				EncodingName: "NetworkCIDRBlock",
+			},
+		},
+	}
+
+	err := flags.Parse(providerVars)
+	if err != nil {
+		return nil, err
+	}
+	return flannelOptions, nil
 }
