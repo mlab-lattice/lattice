764006bf33ae23a923737e69667c5953fb26f833
Kevin Rosendahl <kevindrosendahl@gmail.com>
2017-12-19 23:26:07 -0800

    add nodepool controller

---
+++ cmd/kubernetes/lattice-controller-manager/app/cloudcontrollers/local/local_controllers.go
@@ -0,0 +1,11 @@
+package aws
+
+import (
+	controller "github.com/mlab-lattice/system/cmd/kubernetes/lattice-controller-manager/app/common"
+)
+
+func GetControllerInitializers() map[string]controller.Initializer {
+	return map[string]controller.Initializer{
+		"node-pool": initializeNodePoolController,
+	}
+}
