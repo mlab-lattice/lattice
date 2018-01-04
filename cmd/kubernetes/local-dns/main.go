74a981e98eb722601a6154ae5a68751dc9b27b86
Rob Carlan <rob@objectlabs.com>
2018-01-04 10:25:55 -0800

    simplify local-dns startup code

--- cmd/kubernetes/local-dns/main.go
+++ cmd/kubernetes/local-dns/main.go
@@ -2,29 +2,70 @@ package main
 
 import (
 	"flag"
+	"time"
+
+	controller "github.com/mlab-lattice/system/cmd/kubernetes/lattice-controller-manager/app/common"
+	"github.com/mlab-lattice/system/pkg/backend/kubernetes/cloudprovider/local/controller"
+	latticeinformers "github.com/mlab-lattice/system/pkg/backend/kubernetes/customresource/generated/informers/externalversions"
 
 	kubeconstants "github.com/mlab-lattice/system/pkg/backend/kubernetes/constants"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/golang/glog"
 )
 
 var (
 	kubeconfig          string
 	clusterIDString     string
-	provider            string
-	terraformModulePath string
-	hostConfigPath      string
-	serverConfigPath    string
+	hostsFilePath      string
+	dnsmasqConfigPath    string
 )
 
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to kubeconfig file")
 	flag.StringVar(&clusterIDString, "cluster-id", "", "id of the cluster")
-	flag.StringVar(&provider, "provider", "", "provider to use")
-	flag.StringVar(&terraformModulePath, "terraform-module-path", "/etc/terraform/modules", "path to terraform modules")
-	flag.StringVar(&serverConfigPath, "server-config-path", kubeconstants.DNSSharedConfigDirectory + kubeconstants.DNSConfigFile, "path to the additional dnsmasq cname file")
-	flag.StringVar(&hostConfigPath, "host-config-path", kubeconstants.DNSSharedConfigDirectory + kubeconstants.DNSHostsFile, "path to the additional dnsmasq hosts")
+	flag.StringVar(&dnsmasqConfigPath, "dnsmasq-config-path", kubeconstants.DNSSharedConfigDirectory + kubeconstants.DNSConfigFile, "path to the additional dnsmasq configuration file")
+	flag.StringVar(&hostsFilePath, "hosts-file-path", kubeconstants.DNSSharedConfigDirectory + kubeconstants.DNSHostsFile, "path to the additional dnsmasq hosts")
 	flag.Parse()
 }
 
 func main() {
-	Run(clusterIDString, kubeconfig, provider, terraformModulePath, serverConfigPath, hostConfigPath)
+	var config *rest.Config
+	var err error
+	if kubeconfig == "" {
+		config, err = rest.InClusterConfig()
+	} else {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	stop := make(chan struct{})
+
+	lcb := controller.LatticeClientBuilder{
+		Kubeconfig: config,
+	}
+
+	versionedLatticeClient := lcb.ClientOrDie("shared-latticeinformers")
+	latticeInformers := latticeinformers.NewSharedInformerFactory(versionedLatticeClient, time.Duration(12*time.Hour))
+
+	if err != nil {
+		panic(err)
+	}
+
+	glog.V(1).Info("Starting dns controller")
+
+	go dnscontroller.NewController(
+		dnsmasqConfigPath,
+		hostsFilePath,
+		lcb.ClientOrDie("local-dns-lattice-address"),
+		latticeInformers.Lattice().V1().Endpoints(),
+	).Run(4, stop)
+
+	glog.V(1).Info("Starting informer factory")
+	latticeInformers.Start(stop)
+
+	select {}
 }
