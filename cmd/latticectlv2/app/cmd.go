4c5345e0685f2842f54a4391d3caf81b6163c58c
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-02-27 02:18:48 -0800

    remove dead code

--- cmd/latticectlv2/app/cmd.go
+++ cmd/latticectlv2/app/cmd.go
@@ -5,27 +5,6 @@ import (
 	"github.com/mlab-lattice/system/pkg/cli/command"
 )
 
-//// Cmd represents the base command when called without any subcommands
-//var Cmd = &cobra.BasicCommand{
-//	Use:   "latticectl",
-//	Short: "BasicCommand line utility for interacting with lattice clusters and systems",
-//}
-
-//// Execute adds all child commands to the root command and sets flags appropriately.
-//// This is called by main.main(). It only needs to happen once to the rootCmd.
-//func Execute() {
-//	if err := Cmd.Execute(); err != nil {
-//		fmt.Println(err)
-//		os.Exit(1)
-//	}
-//}
-
-//func init() {
-//	Cmd.AddCommand(cluster.Cmd)
-//	Cmd.AddCommand(definition.Cmd)
-//	Cmd.AddCommand(system.Cmd)
-//}
-
 var Cmd = command.BasicCommand{
 	Name:  "latticectl",
 	Short: "BasicCommand line utility for interacting with lattice clusters and systems",
