3c5398d8ae8319538f5b7c5db1a99796c743c0ef
Kevin Rosendahl <kevindrosendahl@gmail.com>
2018-02-26 22:38:08 -0800

    refactor colon cli support

--- pkg/cli/command/command.go
+++ pkg/cli/command/command.go
@@ -13,8 +13,6 @@ type Command struct {
 	Flags       []Flag
 	Run         func(args []string)
 	Subcommands []Command
-	colon       bool
-	children    map[string]Command
 }
 
 func (c *Command) Execute() {
@@ -27,62 +25,7 @@ func (c *Command) Execute() {
 		c.exit(err)
 	}
 
-	if c.colon {
-		cmd, err = c.addColonCommands()
-		if err != nil {
-			c.exit(err)
-		}
-	}
-
 	c.exit(cmd.Execute())
-}
-
-func (c *Command) ExecuteColon() {
-	c.colon = true
-	c.Execute()
-}
-
-func (c *Command) addColonCommands() (*cobra.Command, error) {
-	root := &cobra.Command{
-		Use: c.Name,
-		Run: func(cmd *cobra.Command, args []string) {
-			c.Run(args)
-		},
-	}
-	subcommands := c.getSubcommands("")
-	for _, subcommand := range subcommands {
-		cmd := &cobra.Command{
-			Use: subcommand.Name,
-			Run: func(cmd *cobra.Command, args []string) {
-				c.Run(args)
-			},
-		}
-		if err := subcommand.addArgs(cmd); err != nil {
-			return nil, fmt.Errorf("error adding args: %v", err)
-		}
-
-		if err := subcommand.addFlags(cmd); err != nil {
-			return nil, fmt.Errorf("error adding flags: %v", err)
-		}
-
-		root.AddCommand(cmd)
-	}
-
-	return root, nil
-}
-
-func (c *Command) getSubcommands(path string) []Command {
-	var subcommands []Command
-	for _, subcommand := range c.Subcommands {
-		subcommand.Name = fmt.Sprintf("%v%v", path, subcommand.Name)
-		subcommands = append(subcommands, subcommand)
-		for _, subsubcommand := range subcommand.getSubcommands(fmt.Sprintf("%v:", subcommand.Name)) {
-			subcommands = append(subcommands, subsubcommand)
-		}
-
-	}
-
-	return subcommands
 }
 
 func (c *Command) validate() error {
@@ -101,14 +44,12 @@ func (c *Command) init() (*cobra.Command, error) {
 		},
 	}
 
-	if !c.colon {
-		if err := c.addArgs(cmd); err != nil {
-			return nil, fmt.Errorf("error adding args: %v", err)
-		}
+	if err := c.addArgs(cmd); err != nil {
+		return nil, fmt.Errorf("error adding args: %v", err)
+	}
 
-		if err := c.addFlags(cmd); err != nil {
-			return nil, fmt.Errorf("error adding flags: %v", err)
-		}
+	if err := c.addFlags(cmd); err != nil {
+		return nil, fmt.Errorf("error adding flags: %v", err)
 	}
 
 	if err := c.addSubcommands(cmd); err != nil {
@@ -166,13 +107,66 @@ func (c *Command) addSubcommands(cmd *cobra.Command) error {
 			return fmt.Errorf("error initializing subcommand %v: %v", c.Name, err)
 		}
 
-		if !c.colon {
-			cmd.AddCommand(subCmd)
-		}
+		cmd.AddCommand(subCmd)
 		names[subcommand.Name] = struct{}{}
 	}
 
 	return nil
+}
+
+func (c *Command) ExecuteColon() {
+	if err := c.validate(); err != nil {
+		c.exit(err)
+	}
+
+	cmd, err := c.initColon()
+	if err != nil {
+		c.exit(err)
+	}
+
+	c.exit(cmd.Execute())
+}
+
+func (c *Command) initColon() (*cobra.Command, error) {
+	root := &cobra.Command{
+		Use: c.Name,
+		Run: func(cmd *cobra.Command, args []string) {
+			c.Run(args)
+		},
+	}
+	for _, subcommand := range c.getSubcommands("") {
+		cmd := &cobra.Command{
+			Use: subcommand.Name,
+			Run: func(cmd *cobra.Command, args []string) {
+				c.Run(args)
+			},
+		}
+		if err := c.addArgs(cmd); err != nil {
+			return nil, fmt.Errorf("error adding args: %v", err)
+		}
+
+		if err := c.addFlags(cmd); err != nil {
+			return nil, fmt.Errorf("error adding flags: %v", err)
+		}
+
+		root.AddCommand(cmd)
+	}
+
+	return root, nil
+}
+
+func (c *Command) getSubcommands(path string) []Command {
+	var subcommands []Command
+	for _, subcommand := range c.Subcommands {
+		subcommand.Name = fmt.Sprintf("%v%v", path, subcommand.Name)
+		subcommands = append(subcommands, subcommand)
+		for _, subsubcommand := range subcommand.getSubcommands(fmt.Sprintf("%v:", subcommand.Name)) {
+			subcommands = append(subcommands, subsubcommand)
+		}
+
+	}
+
+	return subcommands
 }
 
 func (c *Command) exit(err error) {
