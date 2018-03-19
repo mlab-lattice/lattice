30249f3c01791e063dc435ed72bdeb110b0ad894
Rob Carlan <rob@objectlabs.com>
2018-03-19 13:32:07 -0700

    preint builds

--- pkg/cli/latticectl/command/systems/builds/command.go
+++ pkg/cli/latticectl/command/systems/builds/command.go
@@ -118,27 +118,26 @@ func buildsPrinter(builds []types.SystemBuild, format printer.Format) printer.In
 	var p printer.Interface
 	switch format {
 	case printer.FormatDefault, printer.FormatTable:
-		// FIXME :: implement printing
-		headers := []string{"Name", "Definition", "Status"}
+		headers := []string{"ID", "Version", "State"}
 
 		var rows [][]string
-		//for _, build := range builds {
-		//	var stateColor color.Color
-		//	switch build.State {
-		//	case types.SystemStateStable:
-		//		stateColor = color.Success
-		//	case types.SystemStateFailed:
-		//		stateColor = color.Failure
-		//	default:
-		//		stateColor = color.Warning
-		//	}
-		//
-		//	rows = append(rows, []string{
-		//		string(system.ID),
-		//		system.DefinitionURL,
-		//		stateColor(string(system.State)),
-		//	})
-		//}
+		for _, build := range builds {
+			var stateColor color.Color
+			switch build.State {
+			case types.SystemBuildStateSucceeded:
+				stateColor = color.Success
+			case types.SystemBuildStateFailed:
+				stateColor = color.Failure
+			default:
+				stateColor = color.Warning
+			}
+
+			rows = append(rows, []string{
+				string(builds.ID),
+				builds.Version,
+				stateColor(string(builds.State)),
+			})
+		}
 
 		p = &printer.Table{
 			Headers: headers,
