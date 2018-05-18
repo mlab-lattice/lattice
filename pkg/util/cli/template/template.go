46f50fada4f6dbca05a29fddcbc74527b0fe81ed
Rob Carlan <rob@objectlabs.com>
2018-05-18 10:25:11 -0700

    template working, some inconsistencies

--- pkg/util/cli/template/template.go
+++ pkg/util/cli/template/template.go
@@ -8,27 +8,28 @@ import (
 const DefaultTemplateName = "defaultTemplate"
 const DefaultUsageTemplate = "UsageTemplate"
 const DefaultUsageTemplateGrouped = "UsageTemplateGrouped"
+const DefaultHelpTemplateGrouped = "HelpTemplateGrouped"
 
 //FIXME :: Seem to get : rather than " " when running command systems -h, but not when running command -h or command systems:status -h
-var DefaultTemplate = `{{$namePadding:=35}}{{$flagNamePadding:=10}}
+var DefaultTemplate = `
 {{define "Header"}}{{ colored "Usage: " "white" }}{{.CommandPathBinary}}{{.CommandSeparator}}{{if not .IsRunnable }}{{colored "COMMAND" "bold"}}{{end}}{{if .HasFlags}}{{ colored "[FLAGS] " "bold"}}{{else}}{{colored "COMMAND" "bold"}}{{end}}
 {{if not (eq .Short "") }}
     {{colored .Short "bold"}}
 {{end}}
 Type {{.CommandPathBinary}}{{.CommandSeparator}}{{if .HasSubcommands}}{{ colored "[COMMAND] " "bold"}}{{end}}{{colored "-h" "bold"}} for help and examples.{{end}}
 
-{{define "Flags"}}{{if (gt (len .Flags) 0)}}
+{{define "Flags"}}{{if (gt (len .Flags) 0)}}{{ $namePadding := 10 }}
 
 {{colored "Flags:" "white"}} {{range .FlagsSorted}}
     --{{ rpad .GetName $namePadding }} {{if (ne .GetShort "") }} -{{ rpad .GetShort 2 }} {{ .GetUsage }} {{else}} {{rpad " " 4}}{{ .GetUsage }}{{end}}{{end}}{{end}}
 {{end}}
 
-{{define "HelpTemplate"}}{{template "Header" .}}{{template "Flags" .}}
+{{define "HelpTemplate"}}{{template "Header" .}}{{template "Flags" .}}{{ $namePadding := 35 }}
 {{if .HasSubcommands}}{{ colored "Subcommands: " "white" }}{{range .AllSubcommands}}
     {{ colored (rpad .CommandPath $namePadding) "blue" }} {{ colored .Short "gray" }}{{end}}
 {{end}}{{end}}
 
-{{define "HelpTemplateGrouped"}}{{template "Header" .}}{{template "Flags" .}}
+{{define "HelpTemplateGrouped"}}{{template "Header" .}}{{template "Flags" .}}{{ $namePadding := 35 }}
 {{if .HasSubcommands}}{{ colored "Subcommands: " "white" }}
 {{range .SubcommandsByGroup}}
 {{ colored .GroupName "blue" }}: {{range .Commands}}
--- pkg/util/cli/usage_functions.go
+++ pkg/util/cli/usage_functions.go
@@ -8,16 +8,16 @@ import (
 
 // UsageFuncGroupedCommands provides a usage function that groups commands by the subtree that they are located in
 func UsageFuncGroupedCommands(command *Command) error {
-	tmplName := "groupedUsageTemplate"
-	templateToExecute := "UsageTemplateGrouped"
-	return template.TryExecuteTemplate(template.DefaultTemplate, tmplName, templateToExecute, template.DefaultTemplateFuncs, command)
+	tmplName := template.DefaultTemplateName
+	templateToExecute := template.DefaultUsageTemplateGrouped
+	return template.TryExecuteTemplate(tmplName, template.DefaultTemplate, templateToExecute, template.DefaultTemplateFuncs, command)
 }
 
 // HelpFuncGroupedCommands is a help function that groups commands by the subtree that they are located in
 func HelpFuncGroupedCommands(command *Command) {
-	tmplName := "groupedHelpTemplate"
-	templateToExecute := "HelpTemplateGrouped"
-	err := template.TryExecuteTemplate(template.DefaultTemplate, tmplName, templateToExecute, template.DefaultTemplateFuncs, command)
+	tmplName := template.DefaultTemplateName
+	templateToExecute := template.DefaultHelpTemplateGrouped
+	err := template.TryExecuteTemplate(tmplName, template.DefaultTemplate, templateToExecute, template.DefaultTemplateFuncs, command)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
