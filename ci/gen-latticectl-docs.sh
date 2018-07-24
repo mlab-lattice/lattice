34259b047ba715725b7113e836292aa463ca8418
Tim Fogarty <tim@tfogo.com>
2018-07-24 13:43:22 -0700

    Fix versioned upload of latticectl docs

--- ci/gen-latticectl-docs.sh
+++ ci/gen-latticectl-docs.sh
@@ -5,4 +5,8 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-./docgen-binary/docgen --output-docs ./docs-html/latticectl-reference.md --input-docs ./lattice-repo/docs/cli
+TAG_NAME=$(cd src; git rev-parse --short HEAD)
+
+mkdir -p ./docs-html/$(TAG_NAME)
+
+./docgen-binary/docgen --output-docs ./docs-html/$(TAG_NAME)/latticectl-reference.md --input-docs ./lattice-repo/docs/cli
