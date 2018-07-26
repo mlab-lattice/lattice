9acfef24d12bf5d197bb2aa6a3025f0ba122f6e4
Tim Fogarty <tim@tfogo.com>
2018-07-26 11:43:18 -0700

    versioned file

--- ci/gen-latticectl-docs.sh
+++ ci/gen-latticectl-docs.sh
@@ -6,13 +6,13 @@ set -o nounset
 set -o xtrace
 
 cd lattice-repo
-TAG_NAME=$(git rev-parse --short HEAD)
+TAG_NAME=$(git rev-parse --abbrev-ref HEAD)
 cd ..
 
-mkdir -p ./docs-html/$(TAG_NAME)
+mkdir -p ./docs-html/$TAG_NAME
 
-echo "Building DOCS: ./docs-html/$(TAG_NAME)"
+echo "Building DOCS: ./docs-html/$TAG_NAME"
 
-./docgen-binary/docgen --output-docs ./docs-html/$(TAG_NAME)/latticectl-reference.md --input-docs ./lattice-repo/docs/cli
+./docgen-binary/docgen --output-docs ./docs-html/$TAG_NAME/latticectl-reference.md --input-docs ./lattice-repo/docs/cli
 
 #./docgen-binary/docgen --output-docs ./docs-html/latticectl-reference.md --input-docs ./lattice-repo/docs/cli
