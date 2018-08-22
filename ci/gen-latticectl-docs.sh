c18073167cca703884f7d3852e5a02530e7d4649
Tim Fogarty <tim@tfogo.com>
2018-08-22 15:19:15 -0700

    Comment out tar reuse

--- ci/gen-latticectl-docs.sh
+++ ci/gen-latticectl-docs.sh
@@ -19,7 +19,7 @@ tar -xvf latticectl-docs-markdown-bucket/latticectl-docs.tar.gz -C tar-temp
 
 ./docgen-binary/docgen --output-docs ./tar-temp/latticectl-reference-$TAG_NAME.md --input-docs ./lattice-repo/docs/cli
 
-rm ./tar-temp/*
+rm -rf ./tar-temp/*
 
 tar cvzf ./tarball/latticectl-docs.tar.gz ./tar-temp/*
 
