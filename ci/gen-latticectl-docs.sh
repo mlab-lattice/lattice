8655db70bd2ab74a79b369d27e57ecbfb4201575
Tim Fogarty <tim@tfogo.com>
2018-08-23 15:10:15 -0700

    Stop using old tar contents

--- ci/gen-latticectl-docs.sh
+++ ci/gen-latticectl-docs.sh
@@ -13,8 +13,7 @@ echo "Building DOCS: latticectl-referene-$TAG_NAME.md"
 
 mkdir tar-temp
 
-tar -xvf latticectl-docs-markdown-bucket/latticectl-docs-$TAG_NAME.tar.gz
-
 ./docgen-binary/docgen --output-docs ./tar-temp/latticectl-reference-$TAG_NAME.md --input-docs ./lattice-repo/docs/cli
 
+# We use tarballs in case we want to add multiple files in the future
 tar cvzf ./tarball/latticectl-docsi-$TAG_NAME.tar.gz ./tar-temp/*
