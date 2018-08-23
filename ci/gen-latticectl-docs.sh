3baca3e870e52b77ef6ce4360f9051ea418c8b21
Tim Fogarty <tim@tfogo.com>
2018-08-23 15:05:55 -0700

    Clean up doc gen script

--- ci/gen-latticectl-docs.sh
+++ ci/gen-latticectl-docs.sh
@@ -13,8 +13,8 @@ echo "Building DOCS: latticectl-referene-$TAG_NAME.md"
 
 mkdir tar-temp
 
-tar -xvf latticectl-docs-markdown-bucket/latticectl-docs.tar.gz
+tar -xvf latticectl-docs-markdown-bucket/latticectl-docs-$TAG_NAME.tar.gz
 
 ./docgen-binary/docgen --output-docs ./tar-temp/latticectl-reference-$TAG_NAME.md --input-docs ./lattice-repo/docs/cli
 
-tar cvzf ./tarball/latticectl-docs.tar.gz ./tar-temp/*
+tar cvzf ./tarball/latticectl-docsi-$TAG_NAME.tar.gz ./tar-temp/*
