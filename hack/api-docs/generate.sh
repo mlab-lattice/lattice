2c75fe4b10aa9c191e00d66b3753a2c56fa752d5
abdul <abdul@objectlabs.com>
2018-09-11 14:58:13 -0700

    moving docs to a different dir

--- hack/api-docs/generate.sh
+++ hack/api-docs/generate.sh
@@ -1,11 +1,19 @@
 # clone the docs repo if it does not exist
 
-DOCS_DIR=$LATTICE_ROOT/pkg/api/docs
+DOCS_TARGET_DIR=$LATTICE_ROOT/api-docs
 DOCS_REPO_NAME="lattice-api-docs"
 DOCS_REPO_URI="git@github.com:mlab-lattice/$DOCS_REPO_NAME.git"
+DOCS_REPO_DIR=$DOCS_TARGET_DIR/$DOCS_REPO_NAME
+
+# create the docs target dir if it doesn't exist
+if [ ! -d $DOCS_TARGET_DIR ]
+then
+    mkdir -p $DOCS_TARGET_DIR
+fi
+
 
 function clone_or_pull_docs_repo() {
-    cd $DOCS_DIR
+    cd $DOCS_TARGET_DIR
     # check if docs repo exists
     if [ ! -d $DOCS_REPO_NAME ]
     then
@@ -22,10 +30,10 @@ clone_or_pull_docs_repo
 
 # generate swagger from go using swaggo/swag
 # we need to cd into the directory first so that swag tool works properly especially with struct field descriptions
-cd $LATTICE_ROOT/pkg/api && swag init -g server/rest/v1/api.go
+cd $LATTICE_ROOT/pkg/api && swag init -g server/rest/v1/api.go --swagger $DOCS_TARGET_DIR
 
 # generate slate docs from swagger using widdershins
-widdershins $LATTICE_ROOT/pkg/api/docs/swagger/swagger.yaml -o $LATTICE_ROOT/pkg/api/docs/lattice-api-docs/source/index.html.md --language_tabs "shell:shell"
+widdershins $DOCS_TARGET_DIR/swagger.yaml -o $DOCS_REPO_DIR/source/index.html.md --language_tabs "shell:shell"
 
 # build static docs from slate using middleman
-cd $LATTICE_ROOT/pkg/api/docs/lattice-api-docs && bundle exec middleman build --clean --build-dir ../build
+cd $DOCS_REPO_DIR && bundle exec middleman build --clean --build-dir ../build
